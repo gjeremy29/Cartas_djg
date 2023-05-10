a17ff21a1fb49287e0ac01fd2cc324809672404c
gjeremy29 <gjeremy0229@outlook.es.com>
2023-05-10 16:17:58 -0500

    quinto

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,5 +1,16 @@
 import axios from 'axios'
 
-export const getAllTasks = () => {
-    return axios.get('http://localhost:8000/tasks/api/v1/tasks/')
-}
+const tasksApi = axios.create({
+    baseURL: "http://localhost:8000/tasks/api/v1/tasks/",
+});
+
+export const getAllTasks = () => tasksApi.get("/");
+
+export const getTask = (id) => tasksApi.get(`/${id}/`)
+
+export const createTask = (task) => tasksApi.post("/", task);
+
+export const deleteTask = (id) => tasksApi.delete('/${id}')
+
+export const updateTask = (id, task) => tasksApi.put(`/${id}/`,
+task)
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,5 +1,67 @@
+import { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { createTask, deleteTask, updateTask, getTask } from '../api/tasks.api';
+import { useNavigate, useParams } from 'react-router-dom';
+
 export function TaskFormPage() {
+    const { register, handleSubmit, 
+        formState: { errors }, 
+        setValue
+    } = useForm();
+    const navigate = useNavigate();
+    const params = useParams(); 
+
+
+    const onSubmit = handleSubmit(async (data) => {
+        if (params.id) {
+            await updateTask(params.id, data)
+        } else {
+            await createTask(data);
+        }
+
+        navigate("/tasks");
+    });
+
+    useEffect(() => {
+        async function loadTask() {
+            if (params.id) {
+                const {
+                    data: {title, description },
+                  }  = await getTask(params.id);
+                setValue('title', title)
+                setValue('description', description)
+            }
+        }
+        loadTask();
+    }, [])
+
     return (
-        <div>Task Form Page</div>
-    )
+        <div>
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    placeholder="title"
+                    {...register("title", { required: true })}
+                />
+                {errors.title && <span>title is required</span>}
+
+                <textarea
+                    rows="3"
+                    placeholder="Description"
+                    {...register("description", { required: true })}
+                ></textarea>
+                {errors.description && <span>description is required</span>}
+
+                <button>Save</button>
+            </form>
+
+            {params.id && <button onClick={async() => {
+                const accepted = window.confirm('are you sure?')
+                if (accepted) {
+                    await deleteTask(params.id)
+                    navigate("/tasks");
+                }
+            }}>Delete</button>}
+        </div>
+    );
 }
